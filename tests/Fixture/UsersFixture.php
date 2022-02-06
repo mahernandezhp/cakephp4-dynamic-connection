64544df808672110bfa14f5eb7cc13b964c1945c
Marco <marcohernandezhp@gmail.com>
2022-02-06 15:28:06 -0600

    Added User model

---
+++ tests/Fixture/UsersFixture.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsersFixture
+ */
+class UsersFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'email' => 'Lorem ipsum dolor sit amet',
+                'password' => 'Lorem ipsum dolor sit amet',
+                'role' => 'Lorem ipsum dolor ',
+                'created' => '2022-02-06 21:17:09',
+                'modified' => '2022-02-06 21:17:09',
+            ],
+        ];
+        parent::init();
+    }
+}
